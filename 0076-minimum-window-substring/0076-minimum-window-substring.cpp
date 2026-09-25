75ff331d4ae027ff6d6f1f1b437f9dcfbe0adeec
Karan Ramlakhani <karanramlakhani45@gmail.com>
2026-09-26 00:58:21 +0530

    Time: 7 ms (60.05%), Space: 11.5 MB (64.48%) - LeetHub - Time: O(s.size() + t.size()), Space: O(t.size())

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -5,9 +5,10 @@ public:
         int c = t.size();
         int mini = INT_MAX;
         int start = 0;
+        string ans="";
 
         unordered_map<char, int> m1;
-        unordered_map<char, int> m2;
+        
 
         for (int i = 0; i < t.size(); i++) {
             m1[t[i]]++;
@@ -15,33 +16,34 @@ public:
 
         for (int r = 0; r < s.size(); r++) {
             char ch = s[r];
-            m2[ch]++;
-
-            if (m1.count(ch) && m2[ch] <= m1[ch]) {
-                c--;
+           
+            if(m1.count(ch)){
+                if(m1[ch]>0){
+                    c--;
+                }
+                m1[ch]--;
             }
-
-            while (c == 0) {
-                if (r - l + 1 < mini) {
-                    mini = r - l + 1;
-                    start = l;
+            while(c==0){
+                if(r-l+1<mini){
+                    mini=r-l+1;
+                    start=l;
                 }
-
-                char lch = s[l];
-                m2[lch]--;
-
-                if (m1.count(lch) && m2[lch] < m1[lch]) {
-                    c++;
+                char  lch=s[l];
+                if(m1.count(lch)){
+                    m1[lch]++;
+                    if(m1[lch]>0){
+                        c++;
+                    }
                 }
-
                 l++;
+
             }
         }
 
-        if (mini == INT_MAX)
+        if(mini==INT_MAX){
             return "";
-
-        return s.substr(start, mini);
+        }
+        return s.substr(start,mini);
     }
 };
 
